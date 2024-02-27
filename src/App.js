dea7d19be0b4bd2e84ab52aaf35a953e4355e5a8
Abid Kodakkadan <abid@rariti.io>
2024-02-27 10:35:58 -0700

    Added NFT minting and Faucet driping

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-//This is from where our app content come from
+import React, { useState } from "react";
+import HeaderMain from "./Components/HeaderMain/HeaderMain";
+import { APP_ENUM } from "./enums/appEnum";
+import styles from "./styles/app.module.scss";
+import arrowIcon from "./assets/arrowIcon.svg";
+import MintNFT from "./Components/MintNft/MintNFT";
 
-import logo from './logo.svg';
-import Posts from './component/Posts'; // to display the fetched posts components , import it.
-import './App.css';
+const App = () => {
+  const [walletAddress, setWalletAddress] = useState("");
+  const handleOnClickDrip = async () => {
+    //TODO
+    try {
+      const walletDetails = {
+        wallet_address: walletAddress,
+      };
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(walletDetails),
+      };
+      const response = await fetch(
+        process.env.REACT_APP_SERVER_URI + "/get-drip-response",
+        requestOptions
+      );
+      if (response.ok) {
+        //TODO: continue the process
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          "Hey!, Welcome to my-react-app".
-        </p>
-        <h1>React Post Sharer</h1>
-      </header>
-      <Posts/>
+    <div>
+      <HeaderMain setWalletAddress={setWalletAddress} />
+      <div className={styles.getTokenContainer}>
+        <div className={styles.getTokenContent}>{APP_ENUM.getTokenContent}</div>
+        <div className={styles.walletAddressDetails}>
+          <div className={styles.walletAddressKeyAndValue}>
+            <div className={styles.ethAddress}>{APP_ENUM.ethAddress}</div>
+            <div className={styles.walletAddressContainer}>
+              {walletAddress ? walletAddress : APP_ENUM.getStartByWalletConnect}
+            </div>
+          </div>
+          <div className={styles.dripButton} onClick={handleOnClickDrip}>
+            <button className={styles.dripButtonContent}>
+              {APP_ENUM.dripButton}
+              <img src={arrowIcon} alt="arrow-icon" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <MintNFT />
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,8 @@
-//To run the test with react-testing-library
-
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders app paragraph element', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const paraElement = screen.getByText(/"Hey!, Welcome to my-react-app"./i);
-  expect(paraElement).toBeInTheDocument();
-}); // this test that our App successfully displays the paragraph element with the text "Hey!, Welcome to my-react-app".
-  test('renders app title element', () => {
-    render(<App />);
-    const titleElement = screen.getByText(/React Post Sharer/i);
-    expect(titleElement).toBeInTheDocument(); // this test that our App successfully displays the heading or title element with the text "React Post Sharer".
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
-
-//we can run our test with the command "npm run test".
