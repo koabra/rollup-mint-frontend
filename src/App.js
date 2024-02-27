18daea40a625c7d580ab8b2dee2e43f1cc2756d9
Abid Kodakkadan <abid@rariti.io>
2024-02-27 10:12:34 -0700

    basic frontend mockup

---
+++ src/App.js
@@ -0,0 +1,22 @@
+//This is from where our app content come from
+
+import logo from './logo.svg';
+import Posts from './component/Posts'; // to display the fetched posts components , import it.
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          "Hey!, Welcome to my-react-app".
+        </p>
+        <h1>React Post Sharer</h1>
+      </header>
+      <Posts/>
+    </div>
+  );
+}
+
+export default App;
